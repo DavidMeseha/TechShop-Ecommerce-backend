486566407e96ba990d4d7094da893278387f5fd4
David <davidmmyh@gmail.com>
2024-10-26 13:37:20 +0300

    Images Problem debug

--- src/app.ts
+++ src/app.ts
@@ -36,7 +36,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public")));
 app.use("/images", express.static("public/images"));
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", process.env.ORIGIN);
