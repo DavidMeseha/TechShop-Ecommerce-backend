8eaccfe06a061104697c80f794f76d1fccb7f2a5
David <davidmmyh@gmail.com>
2024-10-26 18:28:59 +0300

    Test

--- dist/app.js
+++ dist/app.js
@@ -35,8 +35,8 @@ app.use((0, morgan_1.default)("dev"));
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: false }));
 app.use((0, cookie_parser_1.default)());
-// app.use(express.static(path.join(__dirname, "public")));
-app.use("/images", express_1.default.static("public/images"));
+app.use(express_1.default.static(path_1.default.join(__dirname, "../public")));
+app.use("/images", express_1.default.static("../public/images"));
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", process.env.ORIGIN);
     next();
