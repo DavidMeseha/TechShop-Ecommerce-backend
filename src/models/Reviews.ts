54c5bfa94460b8ef95939fc4f7d62535c71987d3
David <davidmmyh@gmail.com>
2024-11-26 20:17:48 +0200

    ENV beeing published

--- src/models/Reviews.ts
+++ src/models/Reviews.ts
@@ -5,12 +5,17 @@ export interface IProductReviewDocument
   extends IProductReview,
     mongoose.Document<unknown, any, IProductReview> {}
 
-export const ProductReviewSchema = new mongoose.Schema<IProductReviewDocument>({
-  product: { type: mongoose.Schema.Types.ObjectId, ref: "Products" },
-  customer: { type: mongoose.Schema.Types.ObjectId, ref: "Users" },
-  reviewText: String,
-  rating: Number,
-});
+export const ProductReviewSchema = new mongoose.Schema<IProductReviewDocument>(
+  {
+    product: { type: mongoose.Schema.Types.ObjectId, ref: "Products" },
+    customer: { type: mongoose.Schema.Types.ObjectId, ref: "Users" },
+    reviewText: String,
+    rating: Number,
+  },
+  {
+    timestamps: true,
+  }
+);
 
 export default (mongoose.models.Reviews as mongoose.Model<IProductReview>) ||
   mongoose.model<IProductReview>("Reviews", ProductReviewSchema);
