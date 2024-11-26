dd2082384507fea28b1268504083c23e1c39dc64
David <davidmmyh@gmail.com>
2024-11-26 17:29:18 +0200

    Build

--- dist/middlewares/upload.middleware.js
+++ dist/middlewares/upload.middleware.js
@@ -4,13 +4,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const multer_1 = __importDefault(require("multer"));
-const path_1 = __importDefault(require("path"));
-const storage = multer_1.default.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, "./public/images");
-    },
-    filename: function (req, file, cb) {
-        cb(null, Date.now() + path_1.default.extname(file.originalname));
-    },
-});
+// import path from "path";
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "./public/images");
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, Date.now() + path.extname(file.originalname));
+//   },
+// });
+const storage = multer_1.default.memoryStorage();
 exports.default = (0, multer_1.default)({ storage: storage });
--- dist/routes/commonRouter.js
+++ dist/routes/commonRouter.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -6,6 +15,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const upload_middleware_1 = __importDefault(require("../middlewares/upload.middleware"));
 const common_controller_1 = require("../controllers/common.controller");
+const blob_1 = require("@vercel/blob");
 const router = express_1.default.Router();
 router.get("/reviewedIds", common_controller_1.getReviewIds);
 router.get("/followingIds", common_controller_1.getFollowingIds);
@@ -19,14 +29,21 @@ router.delete("/cart/remove/:id", common_controller_1.removeProductFromCart);
 router.post("/changeLanguage/:lang", common_controller_1.changeLanguage);
 router.post("/cart/add/:id", common_controller_1.addProductToCart);
 router.post("/find", common_controller_1.findInAll);
-router.post("/upload", upload_middleware_1.default.single("image"), (req, res) => {
-    var _a;
-    if (!((_a = req.file) === null || _a === void 0 ? void 0 : _a.path))
-        return;
-    const protocol = req.protocol; // 'http' or 'https'
-    const host = req.get("host"); // domain name and port
-    const fullUrl = `${protocol}://${host}`;
-    const imageUrl = fullUrl + req.file.path.replace("public", "");
-    res.status(200).json({ imageUrl });
-});
+router.post("/upload", upload_middleware_1.default.single("image"), (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    if (!req.file)
+        return res.status(400).json({ message: "error" });
+    console.log(req.file);
+    const file = req.file;
+    try {
+        const blob = yield (0, blob_1.put)(file.originalname, file.buffer, {
+            access: "public",
+            token: process.env.FILES_READ_WRITE_TOKEN
+        });
+        return res.status(200).json({ imageUrl: blob.url });
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: "" });
+    }
+}));
 exports.default = router;
