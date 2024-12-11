0ac721cc454f57607f55045725e6a401415ff458
David <davidmmyh@gmail.com>
2024-12-11 21:55:55 +0200

    rebuild

--- src/app.ts
+++ src/app.ts
@@ -29,7 +29,6 @@ import cron from "node-cron";
 
 const origins = process.env.ORIGIN?.split(",") || [];
 var app: Application = express();
-
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
